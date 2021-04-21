d62c48abfcb16b4a087c6769ec719d13c5bec178
Rellyso <rellysondouglas2015@gmail.com>
2021-04-21 09:22:49 -0300

    Gerating index with SPA, SSR or SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,45 @@
-export default function Home() {
+
+export default function Home(props) {
+  // SPA
+  // Executa com javascript, não sendo visível para crawlers
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/episodes')
+  //   .then(response => response.json())
+  //     .then(data => console.log(data))
+  // }, [])
+
   return (
-    <h1>Index</h1>
+    <div>
+      <h1>Index</h1>
+      <p>
+        {JSON.stringify(props.episodes)}
+      </p>
+    </div>
   )
+}
+
+// SSR 
+// executa toda vez que a aplicação é carregada. Gera muitas requisições
+// export async function getServerSideProps() {
+//   const response = await fetch('http://localhost:3333/episodes')
+//   const data = await response.json()
+
+//   return {
+//     props: {
+//       episodes: data
+//     }
+//   }
+// }
+
+// SSG
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8, // 8hrs
+  }
 }
